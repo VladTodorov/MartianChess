39f5dcac40c15003ce31d530640e4f3f703325eb
Vladislav Todorov <email.vladi@gmail.com>
2022-06-18 17:45:56 -0400

    Spawn Pieces into the scene

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -6,10 +6,16 @@ using System.Linq;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Material[] darkSquareMaterial;
+    private int[] board;
+
     public Material[] lightSquareMaterial;
+    public Material[] darkSquareMaterial;
     [SerializeField] private Transform cam;
-    private GameObject[,] board;
+    private GameObject[,] boardObject;
+
+    public GameObject queenObject;
+    public GameObject bishopObject;
+    public GameObject pawnObject;
 
     private readonly int BOARD_LENGTH_X = 4;
     private readonly int BOARD_LENGTH_Y = 8;
@@ -18,23 +24,54 @@ public class NewBehaviourScript : MonoBehaviour
     {
         cam.transform.position = new Vector3((float)BOARD_LENGTH_X/2 - 0.5f, (float)BOARD_LENGTH_Y/2 - 0.5f, -10f);
         GenerateTiles(BOARD_LENGTH_X, BOARD_LENGTH_Y);
-        //GeneratePieces();
+        GeneratePieces();
     }
-
 
     void Update()
     {
         //UsingMouse();
+
+        //gets phone touch input and highlights selected tile
+        if (Input.touchCount > 0)
+        {
+            // Construct a ray from touch coordinates
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(ray, out RaycastHit hit, 50.0f))
+            {
+                SetTileMaterial(hit.collider.gameObject, 1);
+
+                if (currHighlightedTile == null)
+                {
+                    currHighlightedTile = hit.collider.gameObject;
+                }
+                else if (currHighlightedTile != hit.collider.gameObject)
+                {
+                    SetTileMaterial(currHighlightedTile, 0);
+                    currHighlightedTile = hit.collider.gameObject;
+                }
+            }
+            else
+            {
+                if (currHighlightedTile != null)
+                {
+                    SetTileMaterial(currHighlightedTile, 0);
+                }
+            }
+
+        }
+
+
+
     }
 
 
     private void GenerateTiles(int boardWidth, int boardHight)
     {
-        board = new GameObject[boardWidth, boardHight];
+        boardObject = new GameObject[boardWidth, boardHight];
 
         for (int y = 0; y < boardHight; y++)
             for (int x = 0; x < boardWidth; x++)
-                board[x, y] = GenerateTile(x, y);
+                boardObject[x, y] = GenerateTile(x, y);
         
     }
 
@@ -53,6 +90,50 @@ public class NewBehaviourScript : MonoBehaviour
         return tile;
     }
 
+    private void GeneratePieces()
+    {
+        board = new int[]
+        {
+            3, 3, 2, 0,
+            3, 2, 1, 0,
+            2, 1, 1, 0,
+            0, 0, 0, 0,
+
+            0, 0, 0, 0,
+            0, 1, 1, 2,
+            0, 1, 2, 3,
+            0, 2, 3, 3,
+        };
+
+
+        for (int i = 0; i < board.Length; i++)
+        {
+            GeneratePiece(board[i], i);
+        }
+        
+    }
+
+    private void GeneratePiece(int type, int pos)
+    {
+        int x = pos % BOARD_LENGTH_X;
+        int y = pos / BOARD_LENGTH_X;
+
+        
+
+        if (type == 1)
+        {
+            Instantiate(pawnObject, new Vector3(x, y, -1), Quaternion.identity);
+        } else if (type == 2)
+        {
+            Instantiate(bishopObject, new Vector3(x, y, -1), Quaternion.identity);
+        } else if (type == 3)
+        {
+            Instantiate(queenObject, new Vector3(x, y, -1), Quaternion.identity);
+        }
+
+
+    }
+
     private void SetTileMaterial(GameObject tile, int shade)
     {
         int[] tilePos = tile.name.Split(' ').Take(2).Select(int.Parse).ToArray();
@@ -63,6 +144,7 @@ public class NewBehaviourScript : MonoBehaviour
             tile.GetComponent<MeshRenderer>().material = darkSquareMaterial[shade];
 
     }
+
 
 
 
@@ -96,7 +178,5 @@ public class NewBehaviourScript : MonoBehaviour
             }
         }
     }
-
-
 
 }
