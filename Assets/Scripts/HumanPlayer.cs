3a2fa48054531d0782a402d290b04d5e81e8ece8
Vladislav Todorov <email.vladi@gmail.com>
2022-06-26 12:06:47 -0400

    chore: Added Player classes

---
+++ Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanPlayer : Player
+{
+    public HumanPlayer(int num) : base(num)
+    {
+    }
+
+    public override int GetPlayerMove()
+    {
+        throw new System.NotImplementedException();
+    }
+}
